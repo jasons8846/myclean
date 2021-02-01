65ae3ba4e1a03cfb30a0b84e4c7297704b69ebcb
Jason Samuels <JASONS@pepstores.com>
2021-02-01 08:12:33 +0200

    client navbar

---
+++ client/src/components/navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const navbar = () =>{
+   
+        return (
+            <div>
+                <h1>Hello world</h1>
+            </div>
+        );
+    
+}
+
+export default navbar;
